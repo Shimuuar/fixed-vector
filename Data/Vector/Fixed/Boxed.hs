b05ebc3666d7f63b7765bc800778c261af3ca1c4
'Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-10-30 19:06:53 +0400

    Add boxed vectors

---
+++ Data/Vector/Fixed/Boxed.hs
@@ -0,0 +1,86 @@
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+-- |
+-- Boxed vector
+module Data.Vector.Fixed.Boxed (
+    -- * Immutable
+    Vec
+  , Vec2
+  , Vec3
+    -- * Mutable
+  , MVec
+  ) where
+
+import Control.Monad
+import Data.Primitive.Array
+import Prelude hiding (length,replicate,zipWith,map,foldl)
+
+import Data.Vector.Fixed
+import Data.Vector.Fixed.Mutable
+
+
+
+----------------------------------------------------------------
+-- Data type
+----------------------------------------------------------------
+
+-- | Unboxed vector with fixed length
+newtype Vec n a = Vec (Array a)
+
+-- | Mutable unboxed vector with fixed length
+newtype MVec n s a = MVec (MutableArray s a)
+
+type Vec2 = Vec (S (S Z))
+type Vec3 = Vec (S (S (S Z)))
+
+
+
+----------------------------------------------------------------
+-- Instances
+----------------------------------------------------------------
+
+instance (Arity n, Show a) => Show (Vec n a) where
+  show = show . toList
+
+
+type instance Mutable (Vec n) = MVec n
+
+instance (Arity n) => MVector (MVec n) a where
+  lengthM _ = arity (undefined :: n)
+  new = do
+    v <- newArray (arity (undefined :: n)) uninitialised
+    return $ MVec v
+  clone (MVec v) = do
+    r@(MVec u) <- new
+    copyMutableArray v 0 u 0 (arity (undefined :: n))
+    return r
+  unsafeRead  (MVec v) i   = readArray  v i
+  unsafeWrite (MVec v) i x = writeArray v i x
+  {-# INLINE lengthM     #-}
+  {-# INLINE new         #-}
+  {-# INLINE clone       #-}
+  {-# INLINE unsafeRead  #-}
+  {-# INLINE unsafeWrite #-}
+
+instance (Arity n) => IVector (Vec n) a where
+  unsafeFreeze (MVec v)   = do { a <- unsafeFreezeArray v; return $! Vec  a }
+  unsafeThaw   (Vec  v)   = do { a <- unsafeThawArray   v; return $! MVec a }
+  unsafeIndex  (Vec  v) i = indexArray v i
+  {-# INLINE unsafeFreeze #-}
+  {-# INLINE unsafeThaw   #-}
+  {-# INLINE unsafeIndex  #-}
+
+
+
+type instance Dim (Vec n) = n
+
+instance (Arity n) => Vector (Vec n) a where
+  construct = constructVec
+  inspect   = inspectVec
+  {-# INLINE construct #-}
+  {-# INLINE inspect   #-}
+
+uninitialised :: a
+uninitialised = error "Data.Vector.Fixed.Boxed: uninitialised element"
