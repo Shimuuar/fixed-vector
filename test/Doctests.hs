3af46e2bbe21be428a7aeb58a62097080564fa8c
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2013-02-08 00:34:44 +0400

    Add missing file to version contorl

---
+++ test/Doctests.hs
@@ -0,0 +1,13 @@
+module Main
+where
+
+import Test.DocTest
+import System.FilePath.Find ((==?), always, extension, find)
+
+find_sources :: IO [FilePath]
+find_sources = find always (extension ==? ".hs") "Data"
+
+main :: IO ()
+main = do
+  sources <- find_sources
+  doctest $ sources
