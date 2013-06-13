98717c50ed5fca599fa6de2924c9566ef5b2eb44
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2013-06-13 19:05:54 +0400

    Bump version

--- Data/Vector/Fixed/Internal/Id.hs
+++ Data/Vector/Fixed/Internal/Id.hs
@@ -4,8 +4,21 @@ module Data.Vector.Fixed.Internal.Id (
     Id(..)
   ) where
 
+import Control.Applicative (Applicative(..))
+
+
 -- | Strict identity monad
 newtype Id a = Id { runID :: a }
+
+instance Functor Id where
+  fmap f (Id a) = Id (f a)
+  {-# INLINE fmap #-}
+
+instance Applicative Id where
+  pure          = Id
+  Id f <*> Id a = Id (f a)
+  {-# INLINE pure  #-}
+  {-# INLINE (<*>) #-}
 
 instance Monad Id where
   return     = Id
