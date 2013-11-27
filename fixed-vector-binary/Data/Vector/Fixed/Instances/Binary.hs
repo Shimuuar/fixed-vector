41eda0554999047dd5d14d498f9df90e7ecbfe60
'Alexey Khudyakov <alexey.skladnoy@gmail.com>
2013-11-27 12:55:52 +0100

    Add packages for binary/cereal instances

---
+++ fixed-vector-binary/Data/Vector/Fixed/Instances/Binary.hs
@@ -0,0 +1,41 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-- | Module with binary instances for data types defined in fixed
+--   vector
+module Data.Vector.Fixed.Instances.Binary where
+
+import           Data.Vector.Fixed             (Arity)
+import qualified Data.Vector.Fixed           as F
+import qualified Data.Vector.Fixed.Boxed     as B
+import qualified Data.Vector.Fixed.Unboxed   as U
+import qualified Data.Vector.Fixed.Primitive as P
+import qualified Data.Vector.Fixed.Storable  as S
+import           Data.Binary                   (Binary(..))
+
+
+instance (Arity n, Binary a) => Binary (B.Vec n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (Arity n, P.Prim a, Binary a) => Binary (P.Vec n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (Arity n, S.Storable a, Binary a) => Binary (S.Vec n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (U.Unbox n a, Binary a) => Binary (U.Vec n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (Arity n, Binary a) => Binary (F.VecList n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (Binary a) => Binary (F.Only a) where
+  put (F.Only a) = put a
+  get = F.Only `fmap` get
+
+instance Binary (F.Empty a) where
+  put _ = return ()
+  get = return F.Empty
---
+++ fixed-vector-cereal/Data/Vector/Fixed/Instances/Cereal.hs
@@ -0,0 +1,41 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-- | Module with cereal instances for data types defined in fixed
+--   vector
+module Data.Vector.Fixed.Instances.Cereal where
+
+import           Data.Vector.Fixed             (Arity)
+import qualified Data.Vector.Fixed           as F
+import qualified Data.Vector.Fixed.Boxed     as B
+import qualified Data.Vector.Fixed.Unboxed   as U
+import qualified Data.Vector.Fixed.Primitive as P
+import qualified Data.Vector.Fixed.Storable  as S
+import           Data.Serialize                (Serialize(..))
+
+
+instance (Arity n, Serialize a) => Serialize (B.Vec n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (Arity n, P.Prim a, Serialize a) => Serialize (P.Vec n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (Arity n, S.Storable a, Serialize a) => Serialize (S.Vec n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (U.Unbox n a, Serialize a) => Serialize (U.Vec n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (Arity n, Serialize a) => Serialize (F.VecList n a) where
+  put = F.mapM_ put
+  get = F.replicateM get
+
+instance (Serialize a) => Serialize (F.Only a) where
+  put (F.Only a) = put a
+  get = F.Only `fmap` get
+
+instance Serialize (F.Empty a) where
+  put _ = return ()
+  get = return F.Empty
