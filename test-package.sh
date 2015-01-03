032d321a89050f10052f57fb6236f8ca352af6d7
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2015-01-03 21:24:00 +0300

    Ouch didn't fix shell script

--- test-package.sh
+++ test-package.sh
@@ -5,17 +5,17 @@ set -v # Be verbose
 cd "$1"
 
 # Install dependencies, build and run test suite
-cabal-1.18 install   --enable-tests --only-dependencies
-cabal-1.18 configure --enable-tests -v2  # -v2 provides useful information for debugging
-cabal-1.18 build
-cabal-1.18 test
+cabal install   --enable-tests --only-dependencies
+cabal configure --enable-tests -v2  # -v2 provides useful information for debugging
+cabal build
+cabal test
 
 # Check that tarball could be built and installed
-cabal-1.18 sdist
-SRC_TGZ=$(cabal-1.18 info . | awk '{print $2 ".tar.gz";exit}') ;
+cabal sdist
+SRC_TGZ=$(cabal info . | awk '{print $2 ".tar.gz";exit}') ;
 cd dist/
 if [ -f "$SRC_TGZ" ]; then
-    cabal-1.18 install "$SRC_TGZ"
+    cabal install "$SRC_TGZ"
 else
     echo "expected '$SRC_TGZ' not found"
     exit 1
