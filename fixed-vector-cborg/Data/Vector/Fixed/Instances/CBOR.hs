02288d215d4a9c58ebbd146a48a6f5d4b2004e4b
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2017-10-14 21:06:55 +0300

    Add CBOR encoding

---
+++ fixed-vector-cborg/Data/Vector/Fixed/Instances/CBOR.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-- | Module with binary instances for data types defined in fixed
+--   vector
+module Data.Vector.Fixed.Instances.CBOR where
+
+import           Codec.Serialise
+import           Codec.CBOR.Encoding           (Encoding,encodeListLen,encodeNull)
+import           Codec.CBOR.Decoding           (Decoder,decodeListLenOf,decodeNull)
+import           Data.Monoid                   ((<>))
+
+import           Data.Vector.Fixed             (Arity)
+import qualified Data.Vector.Fixed           as F
+import           Data.Vector.Fixed.Cont        (arity,Dim)
+import qualified Data.Vector.Fixed.Boxed     as B
+import qualified Data.Vector.Fixed.Unboxed   as U
+import qualified Data.Vector.Fixed.Primitive as P
+import qualified Data.Vector.Fixed.Storable  as S
+
+
+
+instance (Arity n, Serialise a) => Serialise (B.Vec n a) where
+  encode = encodeFixedVector
+  decode = decodeFixedVector
+
+instance (Arity n, P.Prim a, Serialise a) => Serialise (P.Vec n a) where
+  encode = encodeFixedVector
+  decode = decodeFixedVector
+
+instance (Arity n, S.Storable a, Serialise a) => Serialise (S.Vec n a) where
+  encode = encodeFixedVector
+  decode = decodeFixedVector
+
+instance (U.Unbox n a, Serialise a) => Serialise (U.Vec n a) where
+  encode = encodeFixedVector
+  decode = decodeFixedVector
+
+instance (Arity n, Serialise a) => Serialise (F.VecList n a) where
+  encode = encodeFixedVector
+  decode = decodeFixedVector
+
+instance (Serialise a) => Serialise (F.Only a) where
+  encode = encodeFixedVector
+  decode = decodeFixedVector
+
+instance Serialise (F.Empty a) where
+  encode = const encodeNull
+  decode = F.Empty <$ decodeNull
+
+-- | Encode vector with statically known size as CBOR list. There's no
+--   type tag
+encodeFixedVector :: (F.Vector v a, Serialise a) => v a -> Encoding
+{-# INLINE encodeFixedVector #-}
+encodeFixedVector v = encodeListLen (fromIntegral $ F.length v)
+                   <> F.foldMap encode v
+
+-- | Decode vector with statically known size as CBOR list. There's no
+--   type tag
+decodeFixedVector :: forall v s a. (F.Vector v a, Serialise a) => Decoder s (v a)
+{-# INLINE decodeFixedVector #-}
+decodeFixedVector = do
+  decodeListLenOf (fromIntegral $ arity (undefined :: Dim v))
+  F.replicateM decode
---
+++ fixed-vector-cborg/test/QC.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+
+import Codec.Serialise
+import Data.Typeable
+import           Data.Vector.Fixed             (N2,N3,N4)
+import qualified Data.Vector.Fixed           as F
+import qualified Data.Vector.Fixed.Unboxed   as U
+import qualified Data.Vector.Fixed.Boxed     as B
+import qualified Data.Vector.Fixed.Storable  as S
+import qualified Data.Vector.Fixed.Primitive as P
+import           Data.Vector.Fixed.Instances.CBOR ()
+
+
+
+tests = testGroup "cereal"
+  [ testTagged p_serialize (T :: T (F.Only  Int))
+  , testTagged p_serialize (T :: T (F.VecList N2 Int))
+  , testTagged p_serialize (T :: T (F.VecList N3 Int))
+  , testTagged p_serialize (T :: T (F.VecList N4 Int))
+    --
+  , testTagged p_serialize (T :: T (U.Vec N2 Int))
+  , testTagged p_serialize (T :: T (U.Vec N3 Int))
+  , testTagged p_serialize (T :: T (U.Vec N4 Int))
+    --
+  , testTagged p_serialize (T :: T (B.Vec N2 Int))
+  , testTagged p_serialize (T :: T (B.Vec N3 Int))
+  , testTagged p_serialize (T :: T (B.Vec N4 Int))
+    --
+  , testTagged p_serialize (T :: T (S.Vec N2 Int))
+  , testTagged p_serialize (T :: T (S.Vec N3 Int))
+  , testTagged p_serialize (T :: T (S.Vec N4 Int))
+    --
+  , testTagged p_serialize (T :: T (P.Vec N2 Int))
+  , testTagged p_serialize (T :: T (P.Vec N3 Int))
+  , testTagged p_serialize (T :: T (P.Vec N4 Int))
+  ]
+
+p_serialize :: (Serialise a, Arbitrary a, Eq a) => T a -> a -> Bool
+p_serialize _ a = a == (deserialise . serialise) a
+
+data T a = T
+
+testTagged :: forall a b. (Testable b, Typeable a) => (T a -> b) -> T a -> TestTree
+testTagged prop t
+  = testProperty (show $ typeOf (undefined :: a)) (prop t)
+
+main :: IO ()
+main = defaultMain tests
+
+----------------------------------------------------------------
+instance Arbitrary a => Arbitrary (F.Only a) where
+  arbitrary = F.replicateM arbitrary
+instance Arbitrary a => Arbitrary (F.Empty a) where
+  arbitrary = F.replicateM arbitrary
+
+instance (F.Arity n, Arbitrary a) => Arbitrary (F.VecList n a) where
+  arbitrary = F.replicateM arbitrary
+instance (U.Unbox n a, Arbitrary a) => Arbitrary (U.Vec n a) where
+  arbitrary = F.replicateM arbitrary
+instance (F.Arity n, Arbitrary a) => Arbitrary (B.Vec n a) where
+  arbitrary = F.replicateM arbitrary
+instance (F.Arity n, S.Storable a, Arbitrary a) => Arbitrary (S.Vec n a) where
+  arbitrary = F.replicateM arbitrary
+instance (F.Arity n, P.Prim a, Arbitrary a) => Arbitrary (P.Vec n a) where
+  arbitrary = F.replicateM arbitrary
+
+
+
