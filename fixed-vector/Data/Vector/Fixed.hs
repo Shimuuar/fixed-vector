af284fdd5afb82879861ea4d779e2100440eeda2
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2017-10-21 20:24:08 +0300

    Maybe this will make GHC 7.10 happy

--- fixed-vector/Data/Vector/Fixed.hs
+++ fixed-vector/Data/Vector/Fixed.hs
@@ -352,11 +352,12 @@ instance (Storable a) => Storable (Only a) where
 
 -- | Empty tuple.
 data Empty a = Empty
-  deriving (Show,Eq,Ord, Data)
+  deriving (Show,Eq,Ord)
 -- GHC7.10 wants standalone deriving for some reason:
 -- >    No instance for (Typeable a)
 -- >      arising from the 'deriving' clause of a data type declaration
 deriving instance Typeable a => Typeable (Empty a)
+deriving instance Data     a => Data     (Empty a)
 
 instance Functor Empty where
   fmap _ Empty = Empty
