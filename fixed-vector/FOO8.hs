b15f9e7e81957ee0e9b6b488193f05e4badcc66a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2024-12-16 10:42:34 +0300

    Add benchmark for compiling

---
+++ fixed-vector/FOO8.hs
@@ -0,0 +1,24 @@
+-- |
+{-# OPTIONS_GHC -ddump-timings -ddump-to-file #-}
+module FOO8 where
+
+import Data.Vector.Fixed qualified as F
+import Data.Vector.Fixed.Primitive (Vec)
+
+type N = 8
+
+foo :: Vec N Int -> Vec N Int -> Vec N Int -> Vec N Int -> Int
+foo v1 v2 v3 v4
+  = F.sum v12
+  + F.sum v13
+  + F.sum v14
+  + F.sum v23
+  + F.sum v24
+  + F.sum v34
+  where
+    v12 = F.zipWith (*) v1 v2
+    v13 = F.zipWith (*) v1 v3
+    v14 = F.zipWith (*) v1 v4
+    v23 = F.zipWith (*) v2 v3
+    v24 = F.zipWith (*) v2 v4
+    v34 = F.zipWith (*) v3 v4
